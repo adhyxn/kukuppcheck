bf5b4d1fffd525640bceb043f8eb6deb3d9d3f74
adhyxn <143823737+adhyxn@users.noreply.github.com>
2023-12-10 17:09:04 +0530

    script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "Pata tha bhosdike";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.tenor.com/T40Z2JV3r0MAAAAC/happy-penis.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
